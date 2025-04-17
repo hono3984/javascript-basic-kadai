2174039a304a65087f20ecb0886e2a9b7ac00852
hono3984 <bn8978-hk@outlook.jp>
2025-04-17 23:52:20 +0900

    kadai_mdn_jsを再削除

--- kadai_mdn_js/mdn.js
+++
@@ -1,8 +0,0 @@
-const today = new Date();
-const [month, day, year] = [
-  today.getMonth(),
-  today.getDate(),
-  today.getFullYear(),
-];
-
-console.log (today.getFullYear() +'年',today.getMonth() + '月', today.getDate() + '日', );
